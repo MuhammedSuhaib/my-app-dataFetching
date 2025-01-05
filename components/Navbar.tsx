2cc55b70c77c06945bebc8005c7aa3d99a71f08c
Suhaib Umair <154515857+MuhammedSuhaib@users.noreply.github.com>
2025-01-05 21:50:20 +0500

    img

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,10 +5,7 @@ const Navbar = () => {
     <nav className="bg-blue-500 md:p-4 text-white">
       <div className="flex flex-col md:flex-row justify-between items-center">
         <Link href="/">
-          <img
-            src="https://see.fontimg.com/api/rf5/9YVZK/YTVmYTczZjQyZDhmNGYzZmI0MWEzZWM4N2FlMjhhMmEub3Rm/QXNzaWdubWVudCA3/autumn-flowers.png?r=fs&h=87&w=1000&fg=FBF9F9&bg=353D4B&tb=1&s=87"
-            alt="Calligraphy fonts"
-          />
+        <img src="/7.png" alt="Logo " />
         </Link>
         <ul className="flex gap-4 text-2xl font-semibold">
           <li>
